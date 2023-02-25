c61692e5fe824ed2041c42b6d49cfc777bb98bc5
basitsattar <basit850@gmail.com>
2023-02-25 13:07:39 +0500

    added firebase config

---
+++ src/Firebase/firebase.config.js
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyBK65GuQQWYC0LTH6XZLqb5zzikJyODPi8",
+  authDomain: "linkedin-clone-2ae82.firebaseapp.com",
+  projectId: "linkedin-clone-2ae82",
+  storageBucket: "linkedin-clone-2ae82.appspot.com",
+  messagingSenderId: "562391354564",
+  appId: "1:562391354564:web:e1672ca7b8081402198d91",
+  measurementId: "G-RKGJ9M423R"
+};
+
+export default firebaseConfig;
