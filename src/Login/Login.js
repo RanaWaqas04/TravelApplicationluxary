45773978ccfaa341a2e50021114ec0f901f4f050
basitsattar <basit850@gmail.com>
2023-02-25 16:16:24 +0500

    chnages

--- src/Login/Login.js
+++ src/Login/Login.js
@@ -1,39 +1,38 @@
 import React, { useState } from "react";
-import { Link, useHistory, useLocation } from 'react-router-dom';
-import useAuth from "./../Hooks/useAuth";
-import './Login.css';
+import { Link, useHistory, useLocation } from "react-router-dom";
+// import useAuth from "./../Hooks/useAuth";
+import "./Login.css";
 
 const Login = () => {
-  const { signInWithGoogle,setUser ,loginWithEmailAndPassword, setIsLoading} = useAuth();
+  // const { signInWithGoogle,setUser ,loginWithEmailAndPassword, setIsLoading} = useAuth();
+  const signInWithGoogle = "";
+  const setUser = "";
+  const loginWithEmailAndPassword = "";
+  const setIsLoading = "";
+  const history = useHistory();
+  const location = useLocation();
 
-const history= useHistory()
-const location = useLocation()
+  const url = location.state?.from || "/home";
 
-const url= location.state?.from || "/home"
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
 
-const [email , setEmail]= useState("")
-const [password , setPassword] = useState("")
+  const handleGetEmail = (e) => {
+    setEmail(e.target.value);
+  };
 
-
-const handleGetEmail = (e) =>{
-  setEmail(e.target.value);
-}
-
-const handleGetPassword = (e)=> {
+  const handleGetPassword = (e) => {
     setPassword(e.target.value);
-}
+  };
 
-
-
-
-const handleLoginWithEmailAndPassword=(e)=>{
+  const handleLoginWithEmailAndPassword = (e) => {
     e.preventDefault();
 
-    loginWithEmailAndPassword(email,password)
-    .then((res) => {
-      setIsLoading(true)
+    loginWithEmailAndPassword(email, password)
+      .then((res) => {
+        setIsLoading(true);
         setUser(res.user);
-        history.push(url)
+        history.push(url);
         // ...
       })
       .catch((error) => {
@@ -41,70 +40,85 @@ const handleLoginWithEmailAndPassword=(e)=>{
         const errorMessage = error.message;
       })
       .finally(() => {
-        setIsLoading(false)
-      })
-}
-
-
-
-
+        setIsLoading(false);
+      });
+  };
 
   const handleGoogleLogin = () => {
     signInWithGoogle()
-      .then((res) => 
-        {
-          setIsLoading(true)
-          setUser(res.user)
-          history.push(url)
-        }
-          )
+      .then((res) => {
+        setIsLoading(true);
+        setUser(res.user);
+        history.push(url);
+      })
       .catch((err) => console.log(err))
       .finally(() => {
-        setIsLoading(false)
-      })
+        setIsLoading(false);
+      });
   };
 
   return (
-    <div className="mt-5">
-            <div className="row pt-5">
-    <div className="col-md-3 form">
-      <div className="form-bg">
+    <div className="container">
+      <div className="headline">
+        <h3>Login</h3>
+      </div>
 
-            <div className="headline">
-              <h3>Login</h3>
-            </div>
+      <div className="mb-3">
+        <button
+          onClick={handleGoogleLogin}
+          type="submit"
+          className="btn btn-outline-dark google-btn-custom"
+        >
+          Continue with Google
+        </button>
+      </div>
 
-            <div className="mb-3">
-              <button onClick={handleGoogleLogin} type="submit" className="btn btn-outline-dark google-btn-custom">Continue with Google</button>
-            </div>
-
-        <form onSubmit={handleLoginWithEmailAndPassword}>
-
-            <div><p className="text-center">or</p></div>
-
-            <div className="mb-3">
-              <input onBlur={handleGetEmail} placeholder="Email" type="email" className="form-control form-custom" id="exampleInputEmail1" aria-describedby="emailHelp"/>
-            </div>
-
-            <div className="mb-3">
-              <input onBlur={handleGetPassword} placeholder="Password" type="password" className="form-control form-custom" id="exampleInputPassword1"/>
-            </div>
-
-            <button type="submit" className="btn btn-success custom-btn">Login</button>
-
-            <div className="already-account">
-            <p><a href="/">Forgot password?</a></p>
-            </div>
-
-            <div className="already-account">
-            <p>No account? <Link className="log-link" to="/register">Create one</Link> </p>
-            </div>
-        </form>
+      <form onSubmit={handleLoginWithEmailAndPassword}>
+        <div>
+          <p className="text-center">or</p>
         </div>
-    </div>
 
-    </div>
+        <div className="mb-3">
+          <input
+            onBlur={handleGetEmail}
+            placeholder="Email"
+            type="email"
+            className="form-control form-custom"
+            id="exampleInputEmail1"
+            aria-describedby="emailHelp"
+          />
         </div>
+
+        <div className="mb-3">
+          <input
+            onBlur={handleGetPassword}
+            placeholder="Password"
+            type="password"
+            className="form-control form-custom"
+            id="exampleInputPassword1"
+          />
+        </div>
+
+        <button type="submit" className="btn btn-success custom-btn">
+          Login
+        </button>
+
+        <div className="already-account">
+          <p>
+            <a href="/">Forgot password?</a>
+          </p>
+        </div>
+
+        <div className="already-account">
+          <p>
+            No account?{" "}
+            <Link className="log-link" to="/register">
+              Create one
+            </Link>{" "}
+          </p>
+        </div>
+      </form>
+    </div>
   );
 };
 
